1886c1d788965ef863ef247041cb5f915e8f0e21
Dare ololade <oladare186@gmail.com>
2026-08-29 11:46:40 +0100

    contants combined in one file

---
+++ constants.asm
@@ -0,0 +1,17 @@
+global STDOUT, STDIN, SYS_READ, SYS_WRITE, SYS_OPEN, SYS_CLOSE, SYS_IOCTL, SYS_EXIT, READONLY
+
+;file descriptors
+%define STDOUT 1
+%define STDIN 0
+
+;system call identifiers
+%define SYS_READ 0
+%define SYS_WRITE 1
+%define SYS_OPEN 2
+%define SYS_CLOSE 3
+%define SYS_IOCTL 16 
+%define SYS_EXIT 60
+
+
+;constants parameters
+%define READONLY 0
